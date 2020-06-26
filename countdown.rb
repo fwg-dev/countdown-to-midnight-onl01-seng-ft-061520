a008f9d189cb84b3e7b853e8b0e5d8fe2159fb58
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-26 06:28:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,7 @@
 
 
 def countdown (number)
+<<<<<<< HEAD
 while number > 0
   puts "#{number} SECOND(S)!"
 number -=1
@@ -17,9 +18,26 @@ end
 
 def countdown_with_sleep (number)
 while number > 0
+=======
+
+while number < 12
+  puts "#{number} SECOND(S)!"
+number -=1
+end
+puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (number)
+
+while number < 12
+>>>>>>> cc074916ea0430d60503177e3441854828a83338
   puts "#{number} SECOND(S)!"
   sleep(1)
 number -=1
 end
+<<<<<<< HEAD
 "HAPPY NEW YEAR!"
+=======
+puts "HAPPY NEW YEAR!"
+>>>>>>> cc074916ea0430d60503177e3441854828a83338
 end
