3361a9053fd9a4024565cd99974bfa87c1213136
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-26 06:35:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@
 
 
 def countdown (seconds_to_midnight) #var number = seconds_to_midnight
-while seconds_to_midnight > 0 
+while seconds_to_midnight > 0
   puts "#{seconds_to_midnight} SECOND(S)!"
 seconds_to_midnight -=1
 end
@@ -23,5 +23,3 @@ seconds_to_midnight -=1
 end
 puts "HAPPY NEW YEAR!"
 end
-
-
